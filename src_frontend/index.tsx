bebfda519087807910fc0913a895b052a732fc11
Fabian Keller <github.100.fkeller@spamgourmet.com>
2019-06-16 20:38:22 +0200

    switched fontawesome from JS to CSS for performance reasons

--- src_frontend/index.tsx
+++ src_frontend/index.tsx
@@ -1,7 +1,13 @@
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+/**
+ * This would be the alternative for using fontawesome from JS.
+ * The benefit would be that there is no dependence on serving
+ * webfonts. Unfortunately, the observer technique used by the
+ * fontawesome JS seems to massively slow down DOM modifications.
+ */
+// import '@fortawesome/fontawesome-free/js/fontawesome'
+// import '@fortawesome/fontawesome-free/js/solid'
+// import '@fortawesome/fontawesome-free/js/regular'
+// import '@fortawesome/fontawesome-free/js/brands'
 
 import { createRoot, createState, createEffect, onCleanup } from 'solid-js';
 
