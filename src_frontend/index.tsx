621000d12884b1fe834002bb4ccbcf1b1c8eb0e6
Fabian Keller <github.100.fkeller@spamgourmet.com>
2019-07-17 15:37:24 +0200

    removed leftovers from previous fontawesome usage

--- src_frontend/index.tsx
+++ src_frontend/index.tsx
@@ -1,14 +1,3 @@
-/**
- * This would be the alternative for using fontawesome from JS.
- * The benefit would be that there is no dependence on serving
- * webfonts. Unfortunately, the observer technique used by the
- * fontawesome JS seems to massively slow down DOM modifications.
- */
-// import '@fortawesome/fontawesome-free/js/fontawesome'
-// import '@fortawesome/fontawesome-free/js/solid'
-// import '@fortawesome/fontawesome-free/js/regular'
-// import '@fortawesome/fontawesome-free/js/brands'
-
 // Needs to be imported once to enable lifecycle hooks
 import './dom_lifecycle';
 
